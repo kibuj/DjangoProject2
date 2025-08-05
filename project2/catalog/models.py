6cd523bbf068bbe66a5ce3fe54d395ae478abbac
bogdan <bogdankibuj@gmail.com>
2025-08-05 23:47:37 +0300

    add product model

---
+++ project2/catalog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    price = models.IntegerField()
+    discount_price = models.FloatField()
+    category = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.title + self.price
